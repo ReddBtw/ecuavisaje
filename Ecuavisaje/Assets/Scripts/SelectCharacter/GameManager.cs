6f28f6f63d1ee1a5874cc10db61bbd9ea69328e9
Byron <byron.mena.7@gmail.com>
2022-03-03 16:44:09 -0500

    Seleccionar Personaje
    Abrir y ejecutar escena SelectCharacter, con los siguientes comandos:
    Flechas derecha e izquierda desplazan entre personajes
    Barra espaciadora selecciona personaje y regresa a ventana principal

---
+++ Ecuavisaje/Assets/Scripts/SelectCharacter/GameManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] personajes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Iniciando el juego");
+        Debug.Log(personajes.Length); foreach (GameObject personaje in personajes)
+        {
+            personaje.gameObject.SetActive(true);
+            Debug.Log("Personaje Activado: " + personaje.name);
+            Debug.Log("Personaje Tag: " + personaje.tag);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
--- Ecuavisaje/Assets/Scripts/SelectCharacter/SelectCharacter.cs
+++ Ecuavisaje/Assets/Scripts/SelectCharacter/SelectCharacter.cs
@@ -9,12 +9,12 @@ public class SelectCharacter : MonoBehaviour
     Vector3 current_angle;
     //Jugador màs cercano
     GameObject closest = null;
-
-    bool giroCompleto = false;
     Vector3 angulo_anterior;
 
     /* Personajes disponibles */
-    int totalCharacters = 6;
+    public int totalCharacters = 6;
+    //Objetos en el plano
+    GameObject[] gos;
 
     // Start is called before the first frame update
     void Start()
@@ -27,7 +27,6 @@ public class SelectCharacter : MonoBehaviour
         /* Busqueda de personaje */
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            giroCompleto = false;
             //Actualizo el ángulo de rotación actual
             current_angle = transform.eulerAngles;
             //Actualizo el ángulo de giro
@@ -35,22 +34,11 @@ public class SelectCharacter : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            giroCompleto = false;
             //Actualizo el ángulo de rotación actual
             current_angle = transform.eulerAngles;
             //Actualizo el ángulo de giro
             target_Rot -= new Vector3(0, 360 / totalCharacters, 0);
         }
-        /* Actulizo el ángulo de rotación */
-        current_angle = new Vector3(0, Mathf.LerpAngle(current_angle.y, target_Rot.y, Time.deltaTime * 2), 0);
-        transform.eulerAngles = current_angle;
-        //Marca al personaje más cercano a la cámara
-        //TODO: Que el código se ejecute una sola vez por giro
-        if (!giroCompleto)
-        {
-            GetClosestObject();
-        }
-        //Selecciona personaje
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Debug.Log("---------------Tecla: Espacio---------------");
@@ -61,22 +49,30 @@ public class SelectCharacter : MonoBehaviour
                 GameObject player = GameObject.FindGameObjectWithTag("Player");
                 Debug.Log("Seleccionando personaje: " + player.name);
                 DontDestroyOnLoad(player);
-                SceneManager.LoadScene("DevFun");
+                SceneManager.LoadScene("PrimeraEscena");
             }
         }
+        /* Actulizo el ángulo de rotación */
+        current_angle = new Vector3(0, Mathf.LerpAngle(current_angle.y, target_Rot.y, Time.deltaTime * 2), 0);
+        transform.eulerAngles = current_angle;
+        //Marca al personaje más cercano a la cámara
+        //TODO: Que el código se ejecute una sola vez por giro
+        GetClosestObject();
+        //Selecciona personaje
     }
     /* Objeto con distancia más corta a la cámara */
     public GameObject GetClosestObject()
     {
+        //Jugadores en el plano
+        gos = GameObject.FindGameObjectsWithTag("Personaje");
         //A) Comprobar si existio un cambio con el personaje
         GameObject goAuxiliar = closest;
-        //Jugadores en el plano
-        GameObject[] gos;
-        gos = GameObject.FindGameObjectsWithTag("Personaje");
+
         float distance = Mathf.Infinity;
         /* Poscion de la cámara */
         GameObject camera = GameObject.FindGameObjectWithTag("MainCamera");
         Vector3 position = camera.transform.position;
+
         foreach (GameObject go in gos)
         {
             float distanciaAux = Vector3.Distance(position, go.transform.position);
@@ -86,28 +82,8 @@ public class SelectCharacter : MonoBehaviour
                 closest = go;
             }
         }
-        if (goAuxiliar != closest)
-        {
-            giroCompleto = true;
-        }
         TMPro.TextMeshProUGUI personaje = GameObject.FindGameObjectWithTag("NombrePersonaje").GetComponent<TMPro.TextMeshProUGUI>();
         personaje.text = closest.name;
         return closest;
-    }
-
-    /* Seleccionar personaje */
-    int escena = 0;
-    public void CambioDeEscena()
-    {
-        if (escena == 1)
-        {
-            SceneManager.LoadScene(1);
-            escena--;
-        }
-        else
-        {
-            SceneManager.LoadScene(0);
-            escena++;
-        }
     }
 }
